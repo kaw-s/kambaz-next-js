6839db807d7ae816aa5b2c2dff71de717c68b630
sheena-kaw <skaw2002@gmail.com>
2025-10-09 16:28:53 -0400

    removed quotes

--- app/Labs/Lab2/Positions.tsx
+++ app/Labs/Lab2/Positions.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/no-unescaped-entities */
+
 
 export default function Positions() {
   return (
@@ -46,9 +46,9 @@ export default function Positions() {
       </div>
       <div id="wd-css-position-fixed">
         <h2>Fixed position</h2>
-        Checkout the blue square that says "Fixed position" stuck all the way on
+        Checkout the blue square that says Fixed position stuck all the way on
         the right and half way down the page. It doesn't scroll with the rest of
-        the page. Its position is "Fixed".
+        the page. Its position is Fixed.
         <div
           className="wd-pos-fixed 
     wd-dimension-square wd-bg-color-blue 
