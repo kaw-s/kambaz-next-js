7b4e4d94193b8aa4123ef84716a5fc0d4bf8e8bf
sheena-kaw <skaw2002@gmail.com>
2025-10-16 20:00:39 -0400

    a1

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -1,22 +1,22 @@
-import VariablesAndConstants from "./VariablesAndConstants";
-import VariableTypes from "./VariableTypes";
-import BooleanVariables from "./BooleanVariables";
-import IfElse from "./IfElse";
-import TernaryOperator from "./TernaryOperator";
-import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+// import VariablesAndConstants from "./VariablesAndConstants";
+// import VariableTypes from "./VariableTypes";
+// import BooleanVariables from "./BooleanVariables";
+// import IfElse from "./IfElse";
+// import TernaryOperator from "./TernaryOperator";
+// import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 
 
-export default function Lab3() {
-  return (
-    <div id="wd-lab3">
-      <h3>Lab 3</h3>
-      <VariablesAndConstants />
-      <VariableTypes />
-      <BooleanVariables />
-      <IfElse />
-      <TernaryOperator />
-      <ConditionalOutputIfElse />
+// export default function Lab3() {
+//   return (
+//     <div id="wd-lab3">
+//       <h3>Lab 3</h3>
+//       <VariablesAndConstants />
+//       <VariableTypes />
+//       <BooleanVariables />
+//       <IfElse />
+//       <TernaryOperator />
+//       <ConditionalOutputIfElse />
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
